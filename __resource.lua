b190fc4d37853543c033910e69fea4a9eadab042
Jack Crispy <34707863+JackCrispy@users.noreply.github.com>
2020-03-08 21:47:34 +1100

    Update to support new folders.

--- __resource.lua
+++ __resource.lua
@@ -3,5 +3,10 @@ resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 loadscreen 'src/index.html'
 
 files {
-    "src/*"
+    'src/index.html',
+    'src/song.mp3',
+    'src/css/styles.css',
+    'src/js/basic.js',
+    'src/js/particles.js*',
+    'src/js/maps/particles.js.map'
 }
