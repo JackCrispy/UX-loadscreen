d317040a92ec8a0c8231a30bb1e26c8f2108d266
Jack Crispy <34707863+JackCrispy@users.noreply.github.com>
2020-03-18 12:02:50 +1100

    v2 PUBLIC

--- src/js/main/basic.js
+++ src/js/main/basic.js
@@ -16,9 +16,10 @@ NOTES:
 const config = {
   "useImg": true,//COMING SOON. LEAVE IT :0
   "music": true, //Enable music.
-  "musicID": "lTRiuFIWV54", //ID of the youtube video.
+  "musicID": "TW9d8vYrVFQ", //ID of the youtube video.
   "musicControl": true, //Enable spacebar to toggle music.
   "warning": true, //Enable the warning at top of page.
+  "loadbar": true, //Is the loadbar enabled or not.
   "text": {
     "title": "UX Loadscreen", //Main Title.
     "warningtext": "UX Loadscreen is currently in beta, and should not be used in production. (remove this in config)" //Warning text for the loadscreen.
@@ -32,6 +33,9 @@ const config = {
 window.onload = function reload() {
   if (config.warning === false) {
     document.getElementById("wholediv").style.display = "none";
+  }
+  if (config.useImg === true) {
+    document.getElementById("rgb").style.display = "none";
   }
   document.getElementById('middle-text').innerHTML = config.text.title;
   document.getElementById('warning').innerHTML = config.text.warningtext;
@@ -47,90 +51,125 @@ window.onload = function reload() {
     }
   }
 
-  window.onkeydown = function(e) {
+  window.onkeydown = function (e) {
     var elem = e.target.nodename;
-    if( elem !== 'TEXTAREA' && elem != 'INPUT' ) {
-        return !(e.keyCode == 32);
+    if (elem !== 'TEXTAREA' && elem != 'INPUT') {
+      return !(e.keyCode == 32);
     }
-};
+  };
 
-};
 
-let player;
+  let player;
+  window.onload = function reload1() {
+  if (config.music === true) {
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/player_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-if (config.music) {
-  const tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/player_api";
-  const firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  function onYouTubePlayerAPIReady() {
-    const intId = setInterval(function () {
-      // Check if player is initialized and the video is loaded
-      if (player) {
-        getData()
-        function getData() {
-          document.getElementById("scroll").innerHTML = '<b>🎵: </b>' + player.getVideoData().title;
+    function onYouTubePlayerAPIReady() {
+      const intId = setInterval(function () {
+        // Check if player is initialized and the video is loaded
+        if (player) {
+          getData()
+          function getData() {
+            document.getElementById("scroll").innerHTML = '<b>🎵: </b>' + player.getVideoData().title;
+          }
+          clearInterval(intId);
         }
-        clearInterval(intId);
-      }
-    }, 100);
+      }, 100);
 
-    player = new YT.Player('ytplayer', {
-      width: '0%',
-      height: '0%',
-      videoId: config.musicID,
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
-  }
-
-  function onPlayerReady(event) {
-    event.target.playVideo();
-    player.setPlaybackQuality("small");
-  }
-
-  function onPlayerStateChange(event) {
-    if (player.getPlayerState() == 1) {
-      getData()
-      console.log(title)
+      player = new YT.Player('ytplayer', {
+        width: '100%',
+        height: '100%',
+        videoId: config.musicID,
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
     }
 
-    if (event.data == YT.PlayerState.ENDED) {
-      player.seekTo(0);
-      player.playVideo();
+    function onPlayerReady(event) {
+      event.target.playVideo();
+      player.setPlaybackQuality("small");
+    }
 
-      if (event.data === 0) {
+    function onPlayerStateChange(event) {
+      if (player.getPlayerState() == 1) {
+        getData()
+      }
+
+      if (event.data == YT.PlayerState.ENDED) {
+        player.seekTo(0);
+        player.playVideo();
+
+        if (event.data === 0) {
+          isPaused(false);
+        }
+      }
+    }
+    if (config.musicControl) {
+      document.addEventListener('keyup', event => {
+        if (event.code === 'Space') {
+          toggleAudio();
+        }
+      })
+    }
+
+    function stopAudio() {
+      player.pauseVideo()
+    }
+
+    function playAudio() {
+      player.playVideo()
+    }
+
+    function toggleAudio() {
+      if (player.getPlayerState() == 1 || player.getPlayerState() == 3) {
+        player.pauseVideo();
+        isPasued(true);
+      } else {
+        player.playVideo();
         isPaused(false);
       }
     }
   }
-  if (config.musicControl) {
-    document.addEventListener('keyup', event => {
-      if (event.code === 'Space') {
-        toggleAudio();
-      }
+
+  
+    window.addEventListener('message', function (e) {
+      (handlers[e.data.eventName] || function () { })(e.data);
     })
   }
 
-  function stopAudio() {
-    player.pauseVideo()
-  }
+  if (config.loadbar === false) {
+    document.getElementById("loadbar").style.display = "none";
+  } else {
 
-  function playAudio() {
-    player.playVideo()
-  }
+    //Loader
+    let count = 0;
+    let thisCount = 0;
 
-  function toggleAudio() {
-    if (player.getPlayerState() == 1 || player.getPlayerState() == 3) {
-      player.pauseVideo();
-      isPasued(true);
-    } else {
-      player.playVideo();
-      isPaused(false);
-    }
-  }
-}
 
+    const handlers = {
+      startInitFunctionOrder(data) {
+        count = data.count;
+      },
+
+      initFunctionInvoking(data) {
+        document.querySelector('.load').style.left = '0%';
+        document.querySelector('.load').style.width = ((data.idx / count) * 100) + '%';
+      },
+
+      startDataFileEntries(data) {
+        count = data.count;
+      },
+
+      performMapLoadFunction(data) {
+        ++thisCount;
+        document.querySelector('.load').style.left = '0%';
+        document.querySelector('.load').style.width = ((thisCount / count) * 100) + '%';
+      },
+    };
+  };
+};
